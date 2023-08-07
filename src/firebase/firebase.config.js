d4d9195925f583a64e8ab2293e13d6a817487dd3
siamalrafi <rahathasandipto@gmail.com>
2023-08-07 20:55:10 +0600

    new

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
+   apiKey: process.env.NEXT_PUBLIC_apiKey,
+   authDomain: process.env.NEXT_PUBLIC_authDomain,
+   projectId: process.env.NEXT_PUBLIC_projectId,
+   storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+   messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+   appId: process.env.NEXT_PUBLIC_appId,
 };
 
 // Initialize Firebase
--- src/services/DbConnect.js
+++ src/services/DbConnect.js
@@ -6,26 +6,24 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 let db;
 
 const DbConnect = async () => {
-  if (db) return db;
-  try {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@next-hero.ktnme97.mongodb.net/?retryWrites=true&w=majority`;
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-    db = client.db("easy-shop");
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-    return db;
-  } catch (error) {
-    console.log(error.message);
-  }
+   if (db) return db;
+   try {
+      const uri = `mongodb://localhost:27017`;
+      // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+      const client = new MongoClient(uri, {
+         serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+         },
+      });
+      db = client.db("easy-shop");
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      return db;
+   } catch (error) {
+      console.log(error.message);
+   }
 };
 
 export default DbConnect;
