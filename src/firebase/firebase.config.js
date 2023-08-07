fe28ea272992d6846a2f5a423f6f2afd28b28d92
siamalrafi <rahathasandipto@gmail.com>
2023-08-07 21:08:13 +0600

    new

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyCkNxZ093Gm_Rz8MKWrou7yxr0magLwllM",
-   authDomain: "e-shop-efab9.firebaseapp.com",
-   projectId: "e-shop-efab9",
-   storageBucket: "e-shop-efab9.appspot.com",
-   messagingSenderId: "815590904829",
-   appId: "1:815590904829:web:b82dfccf2b1467f643ff94",
+   apiKey: process.env.NEXT_PUBLIC_apiKey,
+   authDomain: process.env.NEXT_PUBLIC_authDomain,
+   projectId: process.env.NEXT_PUBLIC_projectId,
+   storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+   messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+   appId: process.env.NEXT_PUBLIC_appId,
+
+   // apiKey: "AIzaSyCkNxZ093Gm_Rz8MKWrou7yxr0magLwllM",
+   // authDomain: "e-shop-efab9.firebaseapp.com",
+   // projectId: "e-shop-efab9",
+   // storageBucket: "e-shop-efab9.appspot.com",
+   // messagingSenderId: "815590904829",
+   // appId: "1:815590904829:web:b82dfccf2b1467f643ff94",
 };
 
 // Initialize Firebase
